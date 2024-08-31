8cdde43e5bfcc1432a746c95434c654ac4733f3f
Isaac Cano Orozco <isaac20030122@gmail.com>
2024-08-31 10:04:07 -0500

    Cambios en contar_pares

--- contar_pares.py
+++ contar_pares.py
@@ -1,6 +1,17 @@
-mi_lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+mi_lista = []
 
+# Preguntar al usuario cuántos números quiere ingresar
+cantidad = int(input("¿Cuántos números quieres ingresar?: "))
 
+# Usar un bucle para obtener los números
+for i in range(cantidad):
+    numero = float(input("Ingrese un número: "))
+    mi_lista.append(numero)  # Agregar el número a la lista
+
+# Mostrar la lista de números ingresados
+print("Lista de números ingresados:", mi_lista)
+
+#Funcion que cuenta lo numero pares
 def contar_pares(lista):
     contador = 0
     for numero in lista:
@@ -8,6 +19,6 @@ def contar_pares(lista):
             contador += 1
     return contador
 
-
+#Se llama la funcion y se imprime el resultado
 resultado = contar_pares(mi_lista)
 print(resultado)
