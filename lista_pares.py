d7d38557d4cd381b092c8b4cc4ef71ba2ad48f0e
EdwinCr <efcardenasmo@cesde.net>
2024-09-07 12:41:12 -0500

    Lista pares

---
+++ lista_pares.py
@@ -0,0 +1,12 @@
+lista_completa = []
+lista_pares = []
+i = 1
+while i < 100:
+    lista_completa.append(i)
+    i += 1
+    if(i % 2 == 0):
+        lista_pares.append(i)
+
+print(lista_completa)
+print(lista_pares)
+
